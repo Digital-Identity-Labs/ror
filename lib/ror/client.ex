2c103741d29aa9cc2f9fc961a592550eaeae512d
Pete Birkinshaw <pete@binary-ape.org>
2024-12-29 16:11:52 +0000

    Basic clients for the main API calls

--- lib/ror/client.ex
+++ lib/ror/client.ex
@@ -2,18 +2,82 @@ defmodule ROR.Client do
 
   @default_url "https://api.ror.org/v2/organizations"
 
+  @default_http_options [
+    base_url: @default_url,
+    http_errors: :raise,
+    cache: true,
+    max_redirects: 3,
+    max_retries: 3
+  ]
+
+  @common_options [http: @default_http_options, params: [], api_url: @default_url]
+
+  @default_get_options  @common_options ++ []
+  @allowed_get_options  Keyword.keys(@default_get_options)
+
+  @default_list_options @common_options ++ [page: 1, filter: nil]
+  @allowed_list_options Keyword.keys(@default_list_options)
+
+  @default_query_options @common_options ++ [page: 1, filter: nil]
+  @allowed_query_options Keyword.keys(@default_query_options)
+
+  @default_affiliation_options @common_options ++ []
+  @allowed_affiliation_options Keyword.keys(@default_affiliation_options)
+
   alias ROR.ID
 
-  def http() do
-    Req.new(base_url: @default_url, http_errors: :raise)
+  def http(opts \\ []) do
+    Keyword.merge(@default_http_options, (opts || []))
+    |> Keyword.merge([user_agent: http_agent_name()])
+    |> Req.new()
   end
 
   def get!(id, opts \\ []) do
-    Req.get!(http(), url: ID.path(id)).body
+    opts = Keyword.merge(@default_get_options, (opts || []))
+           |> Keyword.take(@allowed_get_options)
+
+    Req.get!(http(opts[:http]), url: ID.path(id)).body
   end
 
-  def list!(opts \\ [params: []]) do
-    Req.get!(http(), params: opts[:params]).body
+  def list!(opts \\ []) do
+    opts = Keyword.merge(@default_list_options, (opts || []))
+           |> Keyword.take(@allowed_list_options)
+
+    params = Keyword.merge(opts[:params], [page: opts[:page], filter: opts[:filter]])
+
+    Req.get!(http(opts[:http]), params: params).body
+  end
+
+  def query!(value, opts \\ []) do
+    opts = Keyword.merge(@default_query_options, (opts || []))
+           |> Keyword.take(@allowed_query_options)
+
+    params = Keyword.merge(opts[:params], [query: value, page: opts[:page], filter: opts[:filter]])
+
+    Req.get!(http(opts[:http]), params: params).body
+  end
+
+  def query_advanced!(value, opts \\ []) do
+    opts = Keyword.merge(@default_query_options, (opts || []))
+           |> Keyword.take(@allowed_query_options)
+
+    params = Keyword.merge(opts[:params], ["query.advanced": value])
+
+    Req.get!(http(opts[:http]), params: params).body
+  end
+
+  def affiliation!(value, opts \\ [params: []]) do
+    opts = Keyword.merge(@default_affiliation_options, (opts || []))
+           |> Keyword.take(@allowed_affiliation_options)
+
+    params = Keyword.merge(opts[:params], [affiliation: value])
+
+    Req.get!(http(opts[:http]), params: params).body
+  end
+
+  @spec http_agent_name() :: binary()
+  def http_agent_name do
+    "Elixir ROR Client" #{Application.spec(:ror, :vsn)}"
   end
 
 end
