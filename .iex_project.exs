5ce2fa10f8d8bde6dbb796c406c63a741d83dee5
Pete Birkinshaw <pete@binary-ape.org>
2025-01-04 22:50:33 +0000

    Bits. Also update the tools versions

---
+++ .iex_project.exs
@@ -0,0 +1,21 @@
+alias ROR.Admin
+alias ROR.Client
+alias ROR.Domain
+alias ROR.ExternalID
+alias ROR.Filter
+alias ROR.ID
+alias ROR.Link
+alias ROR.Location
+alias ROR.Match
+alias ROR.Matches
+alias ROR.Name
+alias ROR.Organization
+alias ROR.Params
+alias ROR.Relationship
+alias ROR.Results
+alias ROR.Status
+alias ROR.Type
+alias ROR.Utils
+
+
+
