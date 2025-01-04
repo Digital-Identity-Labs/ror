0a06ef00c0fc5eb1cc8f0b90d5199b40e7f47e51
Pete Birkinshaw <pete@binary-ape.org>
2025-01-04 20:36:58 +0000

    Maybe I can sometimes?

--- lib/ror/utils.ex
+++ lib/ror/utils.ex
@@ -29,7 +29,11 @@ defmodule ROR.Utils do
 
   @spec escape_elastic(elastic_string :: binary()) :: binary()
   def escape_elastic(elastic_string) do
-    elastic_string
+    if String.contains?(elastic_string, ":") do
+      elastic_string
+    else
+      String.replace(elastic_string, @elastic_special, fn m -> "\\" <> m end)
+    end
   end
 
 end
